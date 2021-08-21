c5a8c4ebc31cd7fba503d6e50c755114e984bf0d
Sergio Yance <syance@vortexbird.com>
2021-08-21 14:51:27 -0500

    creacion del proyecto inicial

---
+++ src/main/java/com/usb/integrador/ProyectoIntegrador2Application.java
@@ -0,0 +1,13 @@
+package com.usb.integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegrador2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegrador2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/usb/integrador/ProyectoIntegrador2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usb.integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegrador2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
